3e9effc79356a8ac6f4996906d8122f89fef8e67
MhikaelaGalo <mhikaela.galo@ciit.edu.ph>
2026-03-02 01:42:52 +0800

    Refactor server port configuration and logging

--- backend/server.js
+++ backend/server.js
@@ -41,4 +41,8 @@ io.on("connection", (socket) => {
 
 app.get("/", (req, res) => res.send("Socket server running"));
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
